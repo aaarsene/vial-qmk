8fb2a4bb1c7860dc0f6aa377379a53ee88adbef9
Arsène Thieffry <arsene.thieffry@gmail.com>
2021-10-16 22:49:10 +0200

    fix keymap

--- keyboards/cc45/keymaps/via/rules.mk
+++ keyboards/cc45/keymaps/via/rules.mk
@@ -1,3 +1,7 @@
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 # VIAL_INSECURE = yes
+
+CONSOLE_ENABLE = no	# Console for debug
+COMMAND_ENABLE = no    # Commands for debug and configuration
+MIDI_ENABLE = no 		# MIDI controls
